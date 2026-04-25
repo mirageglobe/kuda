46679e09c747b713279f86ccd7190263835fdef6
Jimmy Lim <mirageglobe@gmail.com>
2026-04-25 18:58:19 +0100

    feat: add TLS support for secure connections

--- ui/provider.go
+++ ui/provider.go
@@ -10,8 +10,11 @@ type Server struct {
 // GetServers returns the default list of MUD servers.
 func GetServers() []Server {
 	return []Server{
-		{Name: "Aardwolf", Address: "aardmud.org:23", Desc: "popular fantasy mud"},
+		{Name: "Aardwolf (Secure)", Address: "tls://aardmud.org:443", Desc: "encrypted connection (TLS)"},
+		{Name: "Aardwolf", Address: "aardmud.org:23", Desc: "standard connection"},
 		{Name: "TorilMUD", Address: "torilmud.com:9999", Desc: "forgotten realms mud"},
+		// ...
+
 		{Name: "Genesis MUD", Address: "genesismud.org:3011", Desc: "the original lpmud"},
 		{Name: "Local Echo (dev)", Address: MockAddress, Desc: "in-process echo for testing"},
 	}
